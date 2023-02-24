87844a7e90ae0dd93fa322ee8349e6598d2d7d24
ojpro <contact@ojpro.me>
2023-02-24 11:08:47 +0100

    Initial Project Commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <div className="mx-auto mt-8 shadow bg-white text-center p-4 max-w-fit">
+        <h1 className="text-2xl font-bold text-blue-400"> ☑ Taskolio </h1>
+    </div>
+  </React.StrictMode>,
+)
